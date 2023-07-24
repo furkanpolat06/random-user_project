0dc1019fe942f078443636f4bcc6f1245f379ee0
furkanpolat06 <emnplt.0698@gmail.com>
2023-07-24 12:28:54 +0300

    random user feth api

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import User from "./components/User"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App container text-center mt-4">
+      <User/> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/User.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+const User = () => {
+    const [userData, setUserData] = useState("");
+  const GetUser = () => {
+    fetch("https://randomuser.me/api")
+      .then((res) => res.json())
+      .then((data) => setUserData(data.results[0]))
+      .catch((err) => console.log(err));
+    }
+    useEffect(()=>{
+        const timer =setInterval(GetUser, 100000)
+        GetUser()
+        return()=>{
+            clearInterval(timer)
+        }
+    },[])
+  return (
+    <div>
+      <img className="rounded-circle " src={userData?.picture?.large} alt="" />
+      <h4>Hi, My name is</h4>
+      <h1>{userData?.name?.first} {userData?.name?.last}</h1>
+      <h3>{userData?.email}</h3>
+      <h4>{new Date(userData?.dob?.date).toLocaleDateString("en-US")}</h4>
+      
+      <h5>{userData?.phone}</h5>
+      <h6>{userData?.location?.city}</h6>
+      <button className="btn btn-success" onClick={GetUser}>Get Random User </button>
+    </div>
+  );
+};
+export default User;
