78dce76c75f4239bb06821c08964d338d3c36c2b
furkanpolat06 <emnplt.0698@gmail.com>
2023-07-24 13:14:28 +0300

    style: css edit

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -19,24 +19,29 @@ const User = () => {
     },[])
   return (
     <div>
-      <img className="rounded-circle" src={userData?.picture?.large} alt="" />
-      <h4>Hi, My name is</h4>
-      <h1>{userData?.name?.first} {userData?.name?.last}</h1>
-      <h3>{userData?.email}</h3>
+      {/* <img className="rounded-circle" src={userData?.picture?.large} alt="" /> */}
+      {/* <h4>Hi, My name is</h4>
+      <h1>{userData?.name?.first} {userData?.name?.last}</h1> */}
+      {/* <h3>{userData?.email}</h3>
+      <h4>{new Date(userData?.dob?.date).toLocaleDateString("en-US")}</h4>
+      
+      <h5>{userData?.phone}</h5>
+      <h6>{userData?.location?.city}</h6> */}
+      {/* <button className="btn btn-success" onClick={GetUser}>Get Random User </button> */}
+      <Card className="p-5 container" style={{ width: '40rem' }}>
+      <Card.Img  variant="top" className="rounded-circle " src={userData?.picture?.large} alt="" />
+      <Card.Body>
+        <Card.Title> <h4>Hi, My name is</h4>
+      <h1>{userData?.name?.first} {userData?.name?.last}</h1></Card.Title>
+        <Card.Text>
+        <h3>{userData?.email}</h3>
       <h4>{new Date(userData?.dob?.date).toLocaleDateString("en-US")}</h4>
       
       <h5>{userData?.phone}</h5>
       <h6>{userData?.location?.city}</h6>
-      <button className="btn btn-success" onClick={GetUser}>Get Random User </button>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-      
       
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button className="btn btn-success" onClick={GetUser}>Get Random User </Button>
       </Card.Body>
     </Card>
     </div>
