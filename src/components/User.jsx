c96816d2b9fba384439ba7267ed5436e8b9a64fd
furkanpolat06 <emnplt.0698@gmail.com>
2023-07-25 15:09:49 +0300

    style: bcg color  add

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -19,16 +19,8 @@ const User = () => {
     },[])
   return (
     <div>
-      {/* <img className="rounded-circle" src={userData?.picture?.large} alt="" /> */}
-      {/* <h4>Hi, My name is</h4>
-      <h1>{userData?.name?.first} {userData?.name?.last}</h1> */}
-      {/* <h3>{userData?.email}</h3>
-      <h4>{new Date(userData?.dob?.date).toLocaleDateString("en-US")}</h4>
       
-      <h5>{userData?.phone}</h5>
-      <h6>{userData?.location?.city}</h6> */}
-      {/* <button className="btn btn-success" onClick={GetUser}>Get Random User </button> */}
-      <Card className="p-5 container" style={{ width: '40rem' }}>
+      <Card id='card' className="p-5 container" style={{ width: '40rem' }}>
       <Card.Img  variant="top" className="rounded-circle " src={userData?.picture?.large} alt="" />
       <Card.Body>
         <Card.Title> <h4>Hi, My name is</h4>
